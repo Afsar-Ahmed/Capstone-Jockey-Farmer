cb564aa5c6a76e21689317df367898a8c8636379
Nikita Kartavyi <kartavyi@sheridancollege.ca>
2020-09-30 17:45:48 -0400

    Twicked design, added text resources
    Removed keyboard on start activity as well as focus on the textFields

--- FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/MainActivity.kt
+++ FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/MainActivity.kt
@@ -2,10 +2,14 @@ package sheridan.capstone.findmyfarmer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
     }
 }
