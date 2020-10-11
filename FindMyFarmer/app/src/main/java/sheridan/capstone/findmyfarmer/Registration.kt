0d917d23cbcad688168634d5c205ed7f0e02b9bb
Nikita Kartavyi <kartavyi@sheridancollege.ca>
2020-10-11 17:56:28 -0400

    remove requestFocusTODO

--- FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/Registration.kt
+++ FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/Registration.kt
@@ -24,6 +24,8 @@ class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regristration)
+        
+        TODO("Remove the focus")
         newUserEmail.requestFocus()
         //initialized firebase authentication
         auth = Firebase.auth
