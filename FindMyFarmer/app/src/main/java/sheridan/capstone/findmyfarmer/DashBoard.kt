910940fb656efcbdb45c01ca2f9a415492ed0b9c
Sohaib Hussain <>
2020-10-11 23:56:14 -0400

    Revert "Added Google Sign in , As well as Facebook Sign In. If Commit fdoesnt work on your computer it is due to hash keys missing or your application not registered to the facebook developers app."
    This reverts commit 446eeec7ddf6b96dc205c0010ce97e3c8f0ece6e.

--- FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/DashBoard.kt
+++ FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/DashBoard.kt
@@ -3,15 +3,6 @@ package sheridan.capstone.findmyfarmer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.se.omapi.Session
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
-import com.facebook.login.LoginManager
-import com.firebase.ui.auth.AuthUI
-import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_dash_board.*
@@ -20,11 +11,6 @@ class DashBoard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash_board)
-        
-        var logintext = findViewById<TextView>(R.id.LoginText)
-
-        logintext.text = Firebase.auth.currentUser?.email + " " + Firebase.auth.currentUser?.displayName
-
         logOutBtn.setOnClickListener{
             logOut()
         }
@@ -32,9 +18,7 @@ class DashBoard : AppCompatActivity() {
 
     private fun logOut(){
         Firebase.auth.signOut()
-        LoginManager.getInstance().logOut()
-        AuthUI.getInstance().signOut(this).addOnCompleteListener(){
-            startActivity(Intent(this,Login::class.java))
-        }
+        startActivity(Intent(this,Login::class.java))
+
     }
 }
