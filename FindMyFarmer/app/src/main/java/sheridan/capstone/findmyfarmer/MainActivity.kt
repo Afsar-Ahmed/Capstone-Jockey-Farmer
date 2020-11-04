fe8315cda47edd89d1fa0f8bc3a8049baa4bb7fe
Sohaib Hussain <>
2020-11-04 09:38:19 -0500

    Added Database API calling class (example is present in the MainActivity.kt).  Made an API that is consistently running on our AWS instance.

--- FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/MainActivity.kt
+++ FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/MainActivity.kt
@@ -8,6 +8,9 @@ import android.view.WindowManager
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
+import sheridan.capstone.findmyfarmer.Entities.Customer
+import sheridan.capstone.findmyfarmer.Entities.Farmer
+import sheridan.capstone.findmyfarmer.Entities.Product
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,10 +28,29 @@ class MainActivity : AppCompatActivity() {
         val user = Firebase.auth.currentUser
         if (user != null) {
             startActivity(Intent(this,DashBoard::class.java))
-
         } else {
             startActivity(Intent(this, Login::class.java))
-
         }
+
+        //example
+        val c = DatabaseAPIHandler(this)
+        var d = Customer(1,"Sobi5180","sobi@hotmail.ca","5180")
+        var d1 = Customer(1,"Sobi","sobi5180@hotmail.ca","1234")
+        var d2 = Customer(1,"Sobi","sobi5180@hotmail.ca","1234")
+
+        var f = Farmer(1,"TestBus","Testsestes",10,14)
+        var f1 = Farmer(1,"TestBus","Testsestes",10,16)
+        var f2 = Farmer(1,"TestBus","Testsestes",10,12)
+
+        var p = Product(1,"Rice","Grain")
+        var p1 = Product(1,"Quinoa","Grain")
+        var p2 = Product(1,"Carrot","Vegetable")
+
+        var custlists = listOf<Customer>(d,d1,d2)
+        var flist = listOf<Farmer>(f,f1,f2)
+        var plist = listOf<Product>(p1,p2)
+
+        //This how to call the API
+        //c.execute("/deleteProduct/18");
     }
 }
