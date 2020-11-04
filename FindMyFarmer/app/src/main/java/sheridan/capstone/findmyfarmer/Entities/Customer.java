d86911c75136f188e7943b3fb1475a397b55d3b4
Sohaib Hussain <>
2020-11-04 14:15:19 -0500

    Integrated Database API code and inserted Entities that represent each table in database

---
+++ FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/Entities/Customer.java
@@ -0,0 +1,43 @@
+package sheridan.capstone.findmyfarmer.Entities;
+
+public class Customer {
+
+    private int CustomerID;
+    private String CustomerName;
+    private String CustomerEmail;
+    private String CustomerPassword;
+
+    public Customer() { }
+
+    public Customer(int CustomerID,String CustomerName,String CustomerEmail, String CustomerPassword){
+        this.CustomerID = CustomerID;
+        this.CustomerName = CustomerName;
+        this.CustomerEmail = CustomerEmail;
+        this.CustomerPassword = CustomerPassword;
+    }
+
+    //Setters
+    public void setCustomerEmail(String customerEmail) {
+        CustomerEmail = customerEmail;
+    }
+    public void setCustomerName(String customerName) {
+        CustomerName = customerName;
+    }
+    public void setCustomerPassword(String customerPassword) {
+        CustomerPassword = customerPassword;
+    }
+
+    //Getters
+    public int getCustomerID() {
+        return CustomerID;
+    }
+    public String getCustomerEmail() {
+        return CustomerEmail;
+    }
+    public String getCustomerName() {
+        return CustomerName;
+    }
+    public String getCustomerPassword() {
+        return CustomerPassword;
+    }
+}
