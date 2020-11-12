653c1f512dcd0547b6aec4430de9d3b96dff2df3
Afsar Ahmed <30607335+Afsar-Ahmed@users.noreply.github.com>
2020-11-12 16:33:55 -0500

    I think i managed to update the project ot hte latest version but had to do it manually. Also updated the UI in some activities like login and registration, pending in some more.

--- FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/LoginAndRegistration/View/RegistrationFragment.kt
+++ FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/LoginAndRegistration/View/RegistrationFragment.kt
@@ -16,7 +16,7 @@ class RegistrationFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
         val view = inflater.inflate(R.layout.fragment_registration, container, false)
