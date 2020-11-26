79844411ee33e91f48960c1ab3d783c1eab624d9
Afsar Ahmed <30607335+Afsar-Ahmed@users.noreply.github.com>
2020-11-26 05:16:17 -0500

    so i implemented the api that does save append to the db. I still need to work on the ui as well as look for a better api. Currently the api i have defaulted to is a recipe based api and biggest issue with that is that it does not have an 'all' get request feature. So for the api append i am using a search feature that is dependant on the name of said ingredient and not the id beause they are not consistent

--- FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/Database/ProductAPI.kt
+++ FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/Database/ProductAPI.kt
@@ -10,32 +10,32 @@ import com.android.volley.toolbox.Volley
 import org.json.JSONArray
 import org.json.JSONException
 import sheridan.capstone.findmyfarmer.Entities.Product
+import sheridan.capstone.findmyfarmer.Database.DatabaseAPIHandler
+
 
 class ProductAPI {
 
     val apiKey:String="87cbc6eb7d3548bd9b95d1f715621c20"
     var url:String=""
     var product:Product?=null
-    private var requestQueue: RequestQueue? = null
 
-    fun ProductAPI(){
+    //sets up request to API and what fields that are being called
+     fun apiLOAD(): JsonObjectRequest{
 
-    }
-
-     fun apiLOAD(): JSONArray {
-         url = "https://api.spoonacular.com/food/ingredients/search?apiKey=$apiKey&query=apple"
+       val url = "https://api.spoonacular.com/food/ingredients/search?apiKey=$apiKey&query=apple"
          var productlist = JSONArray()
-        val req = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener {
+        var req = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener {
             response -> try{
             productlist = response.getJSONArray("results")
 
-//            for (i in 1..productlist.length()){
-//                var produce = productlist.getJSONObject(i)
-//
-//                var img = produce.getString("image")
-//                var productName = produce.getString("name")
-//
-//            }
+            for (i in 1..productlist.length()){
+                var produce = productlist.getJSONObject(i)
+
+                var img = produce.getString("image")
+                var productName = produce.getString("name")
+
+
+            }
 
         return@Listener
         } catch (e: JSONException){
@@ -43,7 +43,7 @@ class ProductAPI {
         }
         }, Response.ErrorListener { error -> error.printStackTrace() })
 
-         return productlist
+         return req
     }
 
 
--- FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/MainActivity.kt
+++ FindMyFarmer/app/src/main/java/sheridan/capstone/findmyfarmer/MainActivity.kt
@@ -3,24 +3,36 @@ package sheridan.capstone.findmyfarmer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.farmer_info_card.*
+import kotlinx.android.synthetic.main.fragment_product_management.*
+import org.json.JSONArray
+import org.json.JSONException
 import sheridan.capstone.findmyfarmer.Database.DatabaseAPIHandler
 import sheridan.capstone.findmyfarmer.Entities.Customer
 import sheridan.capstone.findmyfarmer.Entities.Farmer
 import sheridan.capstone.findmyfarmer.Entities.Product
 import sheridan.capstone.findmyfarmer.LoginAndRegistration.Controller.LoginRegistrationController
 import sheridan.capstone.findmyfarmer.Users.CustomerActivity
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
-
+    var requestQueue: RequestQueue? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestQueue = Volley.newRequestQueue(this)
 
         //startActivity(Intent(this, LoginRegistrationController::class.java))
         //Log.d("Switched Activity", "Switched to the LoginRegistrationController")
         checkIfSignedInAccount()
+        storeAPIDataintoDB()
     }
 
     //Checks if the user in signed in the account
@@ -55,5 +67,49 @@ class MainActivity : AppCompatActivity() {
 
         //This how to call the API
        c.execute("/addProducts",plist)
+    }
+
+    //requests access to api
+    fun storeAPIDataintoDB(){
+        val c = DatabaseAPIHandler(this)
+        val apiKey:String="87cbc6eb7d3548bd9b95d1f715621c20"
+        val url = "https://api.spoonacular.com/food/ingredients/search?apiKey=$apiKey&query=apple"
+
+
+
+        var productlist: JSONArray
+        var req = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener {
+                response -> try{
+            productlist = response.getJSONArray("results")
+
+            for (i in 1..productlist.length()){
+                var produce = productlist.getJSONObject(i)
+
+                var id = produce.getInt("id")
+                var img = produce.getString("image")
+                var productName = produce.getString("name")
+
+                product_name.append(productName)
+                produce_catergory.append("")
+
+            convertStringIntoLoad(img)
+        //Fruit_Image.getso
+                //uploads certain values to db
+                c.execute("/addProduct",Product(id,productName,""))
+
+            }
+
+        } catch (e: JSONException){
+            e.printStackTrace()
+        }
+        }, Response.ErrorListener { error -> error.printStackTrace() })
+
+        //after setting up json object, requests call to api
+        requestQueue?.add(req)
+    }
+
+    private fun convertStringIntoLoad(img: String) {
+
+
     }
 }
